761dbd8e3b88a549db71bd7bd003856532cf5e1b
utkarsha-Packt <utkarshak@packt.com>
2020-08-05 21:15:30 +0530

    Removed Chapter08 directory

--- Chapter08/frontend-tracking-dashboard/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static('public'))
-
-app.get('/', (req, res) => res.send('Welcome to DICTAO: Contact tracing web app!'))
-
-app.listen(port, () => console.log(`DICTAO: Contact tracing web app listening at http://localhost:${port}`))
